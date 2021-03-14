228111be8ed4f8b52454c08e7d2fc841add3d2ae
sky <sky012877@gmail.com>
2021-03-14 01:17:54 -0800

    variant2 tested

--- get_url_browzer.py
+++ get_url_browzer.py
@@ -4,10 +4,15 @@
 import pyautogui as pag
 import pyperclip
 
+import time
 import os
 
 import urllib.request
 import requests
+import dryscrape
+import dryscrape.driver.webkit
+import dryscrape.mixins
+
 # TODO new project
 # TODO import dryscrape
 
@@ -32,19 +37,54 @@ pag.hotkey('ctrl', 'c')
 url = pyperclip.paste()
 print(url)
 
+sess = dryscrape.Session()
+sess.set_header("User-Agent", "Mozilla/5.0 (Windows NT 5.1; rv:41.0) Gecko/20100101 Firefox/41.0")
+# we don't need images
+sess.set_attribute('auto_load_images', False)
+sess.set_timeout(100)
+sess.visit(url)
+print(sess.headers())
+sess.render('screenshot.png')
+#
 
-# req = urllib.request.Request(
-#     url,
-#     headers={
-#         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-#     }
-# )
+# # waiting
+sess.wait_for(lambda: sess.at_xpath('/html/body/div[5]'))
+sess.render('screenshot2.png')
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
-# html = urllib.request.urlopen(req).read()
+doc = sess.body()
+print(doc)
 
-response = requests.get(url, headers=headers)
-print(response.text)
-
-# soup = BeautifulSoup(html, 'lxml').text
-# print(soup)
+soup = BeautifulSoup(doc, 'lxml')
+#
+# frame = sess.at_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div/div[2]/div/div[1]/div[1]')
+# # response = sess.body()
+# # <a class="ant-dropdown-link ant-dropdown-trigger">
+#
+# print(frame['src'])
+sess.render('screenshot3.png')
+#
+#
+# # <a class="ant-dropdown-link ant-dropdown-trigger">'
+# sess.render('screenshot2.png')
+#
+# # print(response)
+# # soup = BeautifulSoup(sess.body(), 'lxml')
+print(soup.text)
+#
+# #
+# #
+# # # req = urllib.request.Request(
+# # #     url,
+# # #     headers={
+# # #         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+# # #     }
+# # # )
+# #
+# # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+# # # html = urllib.request.urlopen(req).read()
+# #
+# # response = requests.get(url, headers=headers)
+# # print(response.text)
+# #
+# # soup = BeautifulSoup(html, 'lxml').text
+# # # print(soup)
---
+++ var.py
@@ -0,0 +1,48 @@
+
+from dryscrape.session import Session
+from dryscrape.mixins import WaitTimeoutError
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+
+def new_session():
+    session = Session()
+    session.set_attribute('auto_load_images', False)
+    session.set_header('User-Agent', 'User-Agent", "Mozilla/5.0 (Windows NT 5.1; rv:41.0) Gecko/20100101 Firefox/41.0')
+    return session
+
+
+def session_reset(session):
+    return session.reset()
+
+
+def session_visit(session, url, check):
+    session.visit(url)
+    print('=' * 200)
+    # ensure that the market table is visible first
+    if check:
+        try:
+            print('=' * 200)
+            session.wait_for(lambda: session.at_xpath(
+                '//*[@id="landing-page-app"]/div/div[1]/div[3]/div[1]/div/div/div[2]/div/a[5]/span', timeout=10))
+
+        except WaitTimeoutError:
+            print('=' * 200)
+            pass
+    body = session.body()
+    # session_reset(session)
+    return body
+
+
+SESSION = new_session()
+URL = 'https://leetcode.com/'
+CHECK = True
+
+BODY = session_visit(SESSION, URL, CHECK)
+print(BODY)
+soup = BeautifulSoup(BODY, 'lxml')
+SESSION.render('screenshot2.png')
+
+RESULT = soup.find('div', {'id': 'answer-45824047'})
+
+# print(RESULT)
